9b5de85d11daf405f055a39c298f61d94d7c4bbc
sym <308229387@qq.com>
2025-03-28 16:41:46 +0800

    warninglist

--- app/src/main/java/com/neworange/yujingapp/LoginActivity.kt
+++ app/src/main/java/com/neworange/yujingapp/LoginActivity.kt
@@ -3,21 +3,37 @@ package com.neworange.yujingapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
 class LoginActivity : ComponentActivity() {
+    private var backPressedTime: Long = 0
+    lateinit var loginBtn : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        // 延迟 2 秒后跳转
-        Handler().postDelayed({
-            // 创建跳转 Intent
-            val intent = Intent(this, LoginActivity::class.java)
+        setContentView(R.layout.activity_login)
+        loginBtn = findViewById(R.id.login_btn)
+        loginBtn.setOnClickListener { v: View? ->
+            val intent = Intent(this, WarningListActivity::class.java)
             startActivity(intent)
-            finish() // 关闭当前 Activity（可选，根据需求决定）
-        }, 2000) // 2000 毫秒 = 2 秒
+            finish()
+        }
+    }
+
+    override fun onBackPressed() {
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - backPressedTime < 2000) {
+            super.onBackPressed()
+            finish()
+        } else {
+            // 第一次按下或超时后，更新记录时间并提示用户
+            backPressedTime = currentTime
+            Toast.makeText(this, "再按一次退出应用", Toast.LENGTH_SHORT).show()
+        }
     }
 }
